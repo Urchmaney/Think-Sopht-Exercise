ccd2b983b0f1420929b38c6ca847ab1b7f17a8f6
Urchmaney <kingsobino@gmail.com>
2021-10-15 06:35:39 +0100

    add null checker and more test cases

--- Unegbu_Kingsley/question 2/secondLargestNumber.js
+++ Unegbu_Kingsley/question 2/secondLargestNumber.js
@@ -1,7 +1,7 @@
 const secondLargest = (values) => {
   let largest = -Infinity;
   let sndLargest = -Infinity;
-  values.forEach(val => {
+  values?.forEach(val => {
     if (val > sndLargest) sndLargest = val;
     if (val > largest) {
       sndLargest = largest;
@@ -13,4 +13,12 @@ const secondLargest = (values) => {
 
 
 console.log(secondLargest([10,290,281,32,1,0,817]))
-console.log(secondLargest([-2,-7, -6, -7, -9]))
+console.log(secondLargest([-2,-7, -6, 0, -7, -9]))
+console.log(secondLargest([]));
+console.log(secondLargest([0, 0]));
+console.log(secondLargest([0, 0, 0]));
+console.log(secondLargest([9,8]));
+console.log(secondLargest([9]));
+console.log(secondLargest([Infinity, 8, 8]));
+console.log(secondLargest(null));
+console.log(secondLargest(undefined));
